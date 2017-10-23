df41883a2768cfe76cc57fd539e177355af08b1e
rsevilla223 <rsevilla@luc.edu>
2017-10-23 15:46:25 -0500

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
+import { RouterLink } from '@angular/router/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
+    <h1> Angular CI Example! </h1>
+    <nav>
+      <a routerLink="/red">RED</a>
+      <a routerLink="/blue">BLUE</a>
+    </nav>
     <router-outlet></router-outlet>
-  `,
-  styles: []
+  `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
---
+++ src/app/blue/blue.component.ts
@@ -0,0 +1,11 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blue',
+  template: `
+    <div class="wrapper"></div>
+  `,
+  styles: ['div.wrapper { width: 500px; height: 500px; background-color: blue; }']
+})
+export class BlueComponent {}
